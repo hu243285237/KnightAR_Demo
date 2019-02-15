62c8f73962a4506d0931965acc31089f24190911
hu243285237 <243285237@qq.com>
2019-02-15 10:17:52 +0800

    Add animation when touch screen, and trail fx

---
+++ Assets/MyScript/Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Animator knight;
+
+    public GameObject trail;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                knight.SetTrigger("Attack0" + Random.Range(1, 6));
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            knight.SetTrigger("Attack0" + Random.Range(1, 6));
+        }
+
+        if (knight.GetCurrentAnimatorStateInfo(0).IsName("2Handed_Idle_1"))
+        {
+            trail.SetActive(false);
+        }
+        else 
+        {
+            trail.SetActive(true);
+        }
+    }
+}
